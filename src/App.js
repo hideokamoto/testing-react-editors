b20683fe51a2d39aba6f32bf8f2b9bed9ad69063
hideokamoto <hide.okamoto@digitalcube.jp>
2018-10-16 17:06:20 +0900

    tinymce

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Editor } from '@tinymce/tinymce-react';
 
 class App extends Component {
+  state = {
+    content: "<p>This is the initial content of the editor</p>"
+  }
+  handleEditorChange = (e) => {
+    this.setState({
+      content: e.target.getContent()
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <div dangerouslySetInnerHTML={{__html: this.state.content}} />
+            <Editor
+              initialValue="<p>This is the initial content of the editor</p>"
+              init={{
+                plugins: 'link image code',
+                toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code'
+              }}
+              onChange={this.handleEditorChange}
+            />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>

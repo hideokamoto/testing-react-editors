8662e47801eddd57b5fa26e8f24c3ddc5e49fbdf
hideokamoto <hide.okamoto@digitalcube.jp>
2018-10-23 21:19:15 +0900

    replace libs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Editor } from '@tinymce/tinymce-react';
 
 class App extends Component {
   state = {
@@ -17,28 +16,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <div dangerouslySetInnerHTML={{__html: this.state.content}}/>
           <div style={{width: '800px',margin: 'auto'}}>
-            <Editor
-              initialValue="<p>This is the initial content of the editor</p>"
-              init={{
-                plugins: 'link image code',
-                language: "en",
-                menubar: false,
-                toolbar: 'undo redo | bold italic image | alignleft aligncenter alignright | code',
-                file_picker_types: 'file image media',
-                images_upload_url: 'http://localhost:3000',
-                images_upload_handler: function (blobInfo, success, failure) {
-                  setTimeout(function() {
-                    // no matter what you upload, we will turn it into TinyMCE logo :)
-                    success('https://raw.githubusercontent.com/ask-utils/ask-utils/master/docs/img/logo.png');
-                  }, 2000);
-                },
-                
-                automatic_uploads: true
-              }}
-              onChange={this.handleEditorChange}
-            />
           </div>
           <p>
             Edit <code>src/App.js</code> and save to reload.

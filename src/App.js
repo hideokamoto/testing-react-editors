aff07485b6f0e28eb0bf67d465e02ee9654387e4
hideokamoto <hide.okamoto@digitalcube.jp>
2018-10-23 19:16:21 +0900

    insert gutenberg

--- src/App.js
+++ src/App.js
@@ -1,40 +1,78 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
+import './utils';
+import { data, editPost } from '@frontkom/gutenberg-js';
 
-class App extends Component {
-  state = {
-    content: "<p>This is the initial content of the editor</p>"
-  }
-  handleEditorChange = (e) => {
-    this.setState({
-      content: e.target.getContent()
-    })
+// Gutenberg JS Style
+import '@frontkom/gutenberg-js/build/css/block-library/style.css';
+import '@frontkom/gutenberg-js/build/css/components/style.css';
+import '@frontkom/gutenberg-js/build/css/nux/style.css';
+import '@frontkom/gutenberg-js/build/css/editor/style.css';
+import '@frontkom/gutenberg-js/build/css/block-library/theme.css';
+import '@frontkom/gutenberg-js/build/css/block-library/edit-blocks.css';
+import '@frontkom/gutenberg-js/build/css/style.css';
+
+class Editor extends Component {
+  componentDidMount () {
+    const settings = {
+      alignWide: true,
+      availableTemplates: [],
+      allowedBlockTypes: true,
+      disableCustomColors: false,
+      disablePostFormats: false,
+      titlePlaceholder: 'Add title',
+      bodyPlaceholder: 'Insert your custom block',
+      isRTL: false,
+      autosaveInterval: 0,
+      canPublish: false,
+      canSave: false,
+      canAutosave: false,
+      mediaLibrary: true,
+    };
+
+    // reset localStorage
+    localStorage.removeItem('g-editor-page');
+
+    // Disable tips
+    data.dispatch('core/nux').disableTips();
+
+    // Initialize the editor
+    editPost.initializeEditor('editor', 'page', 1, settings, {});
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div dangerouslySetInnerHTML={{__html: this.state.content}}/>
-          <div style={{width: '800px',margin: 'auto'}}>
-          </div>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div id="editor" className="gutenberg__editor"></div>
     );
   }
 }
+
+class Preview extends Component {
+  state = {
+    rendered: ''
+  }
+  componentDidMount() {
+    const page = JSON.parse(localStorage.getItem('g-editor-page'));
+    if (page) {
+      this.setState({
+        rendered: page.content ? page.content.rendered : '',
+      });
+    }
+  }
+  render () {
+    const { rendered } = this.state;
+    return rendered ? <div dangerouslySetInnerHTML={{__html: rendered}}/> : <center><em>Add your custom block in the editor</em></center>;
+  }
+}
+
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={ Editor } />
+      <Route exact path="/preview" component={ Preview } />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App;

ed49eeef11ff45df12fe80b47a032ae49d1da652
hideokamoto <hide.okamoto@digitalcube.jp>
2018-10-23 23:22:55 +0900

    set example editor

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FroalaEditor from 'react-froala-wysiwyg';
+import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView';
+
+// Require Editor JS files.
+import 'froala-editor/js/froala_editor.pkgd.min.js';
+ 
+// Require Editor CSS files.
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+
+import 'font-awesome/css/font-awesome.css';
+ 
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
 
 class App extends Component {
   state = {
     content: "<p>This is the initial content of the editor</p>"
   }
-  handleEditorChange = (e) => {
+  handleEditorChange = (model) => {
     this.setState({
-      content: e.target.getContent()
+      content: model
     })
   }
   render() {
@@ -17,6 +32,13 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <div style={{width: '800px',margin: 'auto'}}>
+          <FroalaEditor
+            model={this.state.content}
+            onModelChange={this.handleEditorChange}
+            />
+          <FroalaEditorView
+            model={this.state.content}
+          />
           </div>
           <p>
             Edit <code>src/App.js</code> and save to reload.
